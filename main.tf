9504c6f39e488bc1f6e1ff75a764811bae1dca3a
Mohamed Aslam M <mohamedaslamcodes@gmail.com>
2024-10-30 21:14:35 +0530

    update git ref hash value

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
-locals {
-  commit_hash = "4b236a32038fd5bc5ecd08f75c7906da59245915"
-}
-
 module "vpc_1" {
-
-
-  source                = "git::ssh://git@gitlab.presidio.com/mohammedaslamm/tf-project.git//modules/vpc?ref=4df293531b2dc7ef8263d195750d71d724c88027"
+  source                = "git::ssh://git@gitlab.presidio.com/mohammedaslamm/tf-project.git//modules/vpc?ref=9162f638f9770a3776abed4eaa431e6fd4e96820"
   azs                   = ["us-east-1a", "us-east-1b"]
   cidr_block            = "10.0.0.0/16"
   private_subnets_CIDRs = ["10.0.0.0/24", "10.0.1.0/24"]
@@ -19,7 +13,7 @@ module "vpc_1" {
 module "vpc_2" {
 
 
-  source = "git::ssh://git@gitlab.presidio.com/mohammedaslamm/tf-project.git//modules/vpc?ref=4df293531b2dc7ef8263d195750d71d724c88027"
+  source = "git::ssh://git@gitlab.presidio.com/mohammedaslamm/tf-project.git//modules/vpc?ref=9162f638f9770a3776abed4eaa431e6fd4e96820"
 
   azs                   = ["us-west-2a", "us-west-2b"]
   cidr_block            = "10.1.0.0/16"
@@ -33,7 +27,7 @@ module "vpc_2" {
 
 module "vpc_peering" {
   depends_on = [module.vpc_1, module.vpc_2]
-  source     = "git::ssh://git@gitlab.presidio.com/mohammedaslamm/tf-project.git//modules/vpc_peering_w_routes?ref=4df293531b2dc7ef8263d195750d71d724c88027"
+  source     = "git::ssh://git@gitlab.presidio.com/mohammedaslamm/tf-project.git//modules/vpc_peering_w_routes?ref=9162f638f9770a3776abed4eaa431e6fd4e96820"
 
   main_rtb_id    = module.vpc_1.public_rtb_id
   peering_rtb_id = module.vpc_2.public_rtb_id
@@ -55,11 +49,11 @@ module "vpc_peering" {
 
 
 module "iam_role" {
-  source = "git::ssh://git@gitlab.presidio.com/mohammedaslamm/tf-project.git//modules/iam_role?ref=4df293531b2dc7ef8263d195750d71d724c88027"
+  source = "git::ssh://git@gitlab.presidio.com/mohammedaslamm/tf-project.git//modules/iam_role?ref=9162f638f9770a3776abed4eaa431e6fd4e96820"
 }
 
 module "rds" {
-  source        = "git::ssh://git@gitlab.presidio.com/mohammedaslamm/tf-project.git//modules/rds?ref=4df293531b2dc7ef8263d195750d71d724c88027"
+  source        = "git::ssh://git@gitlab.presidio.com/mohammedaslamm/tf-project.git//modules/rds?ref=9162f638f9770a3776abed4eaa431e6fd4e96820"
   db_subnets    = module.vpc_1.public_subnet_ids
   vpc_id        = module.vpc_1.vpc_id
   ingress_cidrs = [module.vpc_1.cidr, module.vpc_2.cidr]
@@ -69,7 +63,7 @@ module "rds" {
 }
 
 module "ec2-1" {
-  source                = "git::ssh://git@gitlab.presidio.com/mohammedaslamm/tf-project.git//modules/ec2?ref=4df293531b2dc7ef8263d195750d71d724c88027"
+  source                = "git::ssh://git@gitlab.presidio.com/mohammedaslamm/tf-project.git//modules/ec2?ref=9162f638f9770a3776abed4eaa431e6fd4e96820"
   instance_type         = "t2.medium"
   rds_endpoint          = module.rds.rds_endpoint
   instance_count        = 1
@@ -82,7 +76,7 @@ module "ec2-1" {
 }
 
 module "ec2-2" {
-  source                = "git::ssh://git@gitlab.presidio.com/mohammedaslamm/tf-project.git//modules/ec2?ref=4df293531b2dc7ef8263d195750d71d724c88027"
+  source                = "git::ssh://git@gitlab.presidio.com/mohammedaslamm/tf-project.git//modules/ec2?ref=9162f638f9770a3776abed4eaa431e6fd4e96820"
   instance_type         = "t2.medium"
   rds_endpoint          = module.rds.rds_endpoint
   instance_count        = 1
