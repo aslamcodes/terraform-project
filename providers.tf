61753f68bcd1fc27ae738d3aef78b87e53d1c7f0
Mohamed Aslam M <mohamedaslamcodes@gmail.com>
2024-11-03 17:31:04 +0530

    Default tags for cost allocation

--- providers.tf
+++ providers.tf
@@ -1,9 +1,21 @@
 provider "aws" {
   alias  = "east"
   region = "us-east-1"
+  default_tags {
+    tags = {
+      owner   = "Mohamed Aslam"
+      project = "Cross region vpc peering project"
+    }
+  }
 }
 
 provider "aws" {
   alias  = "west"
   region = "us-west-2"
+  default_tags {
+    tags = {
+      owner   = "Mohamed Aslam"
+      project = "Cross region vpc peering project"
+    }
+  }
 }
